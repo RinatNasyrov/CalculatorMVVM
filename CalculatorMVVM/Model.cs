fee9caf6400cd50a9760b31a06e742d65ad93c38
RinatNasyrov <rinatpingvin63rus@gmail.com>
2020-02-02 20:28:10 +0400

    Add files via upload

---
+++ CalculatorMVVM/Model.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace CalculatorMVVM
+{
+    class Model : INotifyPropertyChanged
+    {
+        private string outerstring;
+        private double result;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public string OuterString { 
+            get { return outerstring; } 
+            private set 
+            {
+                outerstring = value;
+                NotifyPropertyChanged();
+            } 
+        }
+
+        public double Result
+        {
+            get { return result; }
+            private set 
+            { 
+                result = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        public void AddString(string str)
+        { OuterString += str; }
+
+        public void RemoveLastChar()
+        { OuterString = OuterString.Remove(OuterString.Length); }
+
+        public void ClearString()
+        { OuterString = string.Empty; }
+
+        public void CalcResult()
+        {
+            try 
+            {
+                Result = double.Parse(OuterString);
+            } catch (FormatException) { OuterString = "Что-то не так"; }
+        }
+    }
+}
---
+++ CalculatorMVVM/ViewModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+
+using System.Windows.Forms;
+
+namespace CalculatorMVVM
+{
+    class ViewModel : INotifyPropertyChanged
+    {
+        Model model;
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public ViewModel()
+        {
+            model = new Model();
+            model.PropertyChanged += (s, e) => NotifyPropertyChanged(e.PropertyName);
+
+            TypeCommand = new RoutedCommand();
+        }
+
+        private void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public string OuterString { get { return model.OuterString; } }
+
+        public double Result { get { return model.Result; } }
+
+        public RoutedCommand TypeCommand { get; }
+
+        public void EnterStr(object sender, ExecutedRoutedEventArgs e)
+        { model.AddString(e.Parameter.ToString()) ; }
+    }
+}
